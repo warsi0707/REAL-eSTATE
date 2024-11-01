9e9ac3f12c404d196820e10c6321d690d7841c19
Samir Warsi <warsisamir78.com>
2024-11-01 21:40:15 +0530

    adding frontend link

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://real-estate-2oiv.onrender.com"],
     credentials: true
 }))
 
