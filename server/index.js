0cdb1c096606d59304c58bb0d5e6faed1c167277
Samir Warsi <warsisamir78.com>
2024-11-02 23:34:19 +0530

    new changes

--- server/index.js
+++ server/index.js
@@ -16,8 +16,8 @@ app.use(cookieParser())
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    // origin: "https://real-estate-2oiv.onrender.com",
-    origin: [" http://localhost:5173"],
+    origin: "https://real-estate-2oiv.onrender.com",
+    // origin: [" http://localhost:5173"],
     methods: ["GET","POST","PUT","DELETE"],
     credentials: true
 }))
