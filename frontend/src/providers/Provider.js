8bc5bc520463a166107b70856d3c347b4c6ca511
Samir Warsi <warsisamir78.com>
2025-02-09 23:32:00 +0530

    adding backend url

--- frontend/src/providers/Provider.js
+++ frontend/src/providers/Provider.js
@@ -1,1 +1,2 @@
-export const BackendUrl = "http://localhost:3000/api/v1"
+// export const BackendUrl = "http://localhost:3000/api/v1"
+export const BackendUrl = "https://real-estate-iin9.onrender.com/api/v1"
