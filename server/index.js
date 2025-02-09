e69a3b3156fc205710e6e744b2d924af18c5ccaa
Samir Warsi <warsisamir78.com>
2025-02-09 23:43:28 +0530

    adding backend url

--- server/index.js
+++ server/index.js
@@ -30,7 +30,7 @@ app.use("/api/v1/property", propertyRoute)
 app.use("/api/v1/user", userRouter)
 
 app.get("*",(req, res) =>{
-    res.sendFile(path.resolve(__dirname,'frontend','dist','index.html'))
+    res.sendFile(path.join(__dirname,'frontend','dist','index.html'))
 })
 
 const Main =async()=>{
