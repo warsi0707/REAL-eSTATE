0c55074c75f1b19a3eb2a16514f3f9becc7db175
samir <samir.warsi2001@gmail.com>
2025-03-23 21:45:31 +0530

    changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -12,7 +12,8 @@ export default function Navbar({onClose}) {
      <div className='mx-auto bg-blue-500 max-w-[1300px] py-7 rounded-b-full px-10 flex justify-between sticky top-0'>
       <Link to={"/"} className="ml-8 w-28 sm:w-40"><img src="/housing.png" alt="" /></Link>
       <div className="gap-5 mr-10 text-xl text-white sm:flex">
-        {isUserLogin? <button onClick={logout} >Logout </button>: <button onClick={onClose}>Login </button>}
+        {isUserLogin&& <button onClick={logout} >Logout </button> }
+        {!isUserLogin && <button onClick={onClose}>Login </button>}
       </div>
     </div>   
     </>
